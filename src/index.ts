8b8908706d73be3ae4ddb5ceb5dd9c0aa0eb0f3b
Jaindu Demitha <jaindudemitha565@gmail.com>
2026-07-08 15:17:14 +0530

    WebSocket2

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import express from "express";
 import user from "./routes/user";
 import chat from "./routes/chat";
 import chatHistory from "./routes/chat-history";
+import http from "http";
+import { startWebSocket } from "./webSocket";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,10 +19,15 @@ app.get("/",(req,res)=>{
 
 app.use("/user",user);
 app.use("/chat",chat);
-app.use("/chat-history",chatHistory)
+app.use("/chat-history",chatHistory);
+
+const server = http.createServer(app);
+
+startWebSocket(server);
 
 
-app.listen(3000,()=>{
+//app.listen
+server.listen(3000,()=>{
 console.log("API Started...");
 console.log("API URL: http://localhost:3000");
 
---
+++ src/webSocket.ts
@@ -0,0 +1,65 @@
+import { Server } from "http";
+import { WebSocketServer } from "ws";
+import db from "./db";
+
+export function startWebSocket(server: Server){
+
+    const userConnections = new Map();
+
+    const wsServer = new WebSocketServer({server});
+
+    wsServer.on("connection",(ws)=>{
+        console.log("connected to webSocket");
+
+        ws.on("message", (data)=>{
+
+            const msgData = JSON.parse(data.toString());
+
+             if (msgData.type === "register") {
+                //save to map
+                userConnections.set(msgData.data,ws);
+                console.log("Connection saved!"); 
+
+             } else if (msgData.type === "chat") {
+                //sent to reciver
+
+console.log(msgData);
+                
+                const {data, receiver, sender, chatId} = msgData;
+                
+               const receiverWs = <WebSocket> userConnections.get(receiver);
+
+
+//save to Db
+               const pool = db.promise();
+
+              try {
+
+                 pool.query("INSERT INTO chat_history (message, sent_at, chat_chat_id,sender, msg_status_id) VALUES (?,?,?,?,?)",
+                [data, new Date(),chatId, sender,1 ],
+               );
+
+              } catch (error) {
+               console.log(error); 
+              }
+            
+               //send to receiver
+               if (receiverWs) {
+
+                const msgData ={
+                    message:data,
+                    sent_at:new Date().toISOString(),
+                    sender:sender
+                };
+                
+
+                receiverWs.send(JSON.stringify(msgData));
+                console.log("Msg Sent");
+               }
+            }
+                
+             
+        })
+    });
+
+}
